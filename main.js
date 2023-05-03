b5023b5c3d899210125836974937c058aeab0730
Csongor480 <kovacscsongor07@gmail.com>
2023-05-03 19:54:16 +0200

    6. feladat

--- main.js
+++ main.js
@@ -31,5 +31,24 @@ function showCircuit() {
 //---------------------------------------------------------
 
 function calculate() {
-    
+    let laptime = document.getElementById("laptime").value / 3600
+    let palya = document.getElementById("circuit").value
+    let palyaHossz;
+    switch (palya) {
+      case "HUN":
+        palyaHossz = 4.381;
+        break;
+      case "MON":
+          palyaHossz =  3.337;
+          break;
+      case "BEL":
+        palyaHossz = 7.004;
+        break;
+      case "ITA":
+        palyaHossz = 5.793;
+        break;
+      default:
+        break;
+    }
+    document.getElementById("averagespeed").value = palyaHossz/ laptime
   }
